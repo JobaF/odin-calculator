0d8503c1d69c07ddf921ab0e99a91e585199ffd0
Jabo Fecht <jifecht@gmail.com>
2022-02-13 17:57:11 +0100

    Add bg colors

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
 const initializeCalculatorHTML = () => {
   const calculatorDiv = document.querySelector('.calculator')
-  const symbols = [
-    'AC',
-    '±',
-    '%',
-    '÷',
-    '7',
-    '8',
-    '9',
-    '×',
-    '4',
-    '5',
-    '6',
-    '-',
-    '1',
-    '2',
-    '3',
-    '+',
-    '0',
-    ',',
-    '=',
+  const calculatorProperties = [
+    { name: 'AC', color: 'rgb(57,55,59)' },
+    { name: '±', color: 'rgb(57,55,59)' },
+    { name: '%', color: 'rgb(57,55,59)' },
+    { name: '÷', color: 'rgb(240,155,44)' },
+    { name: '7', color: 'rgb(89,88,92)' },
+    { name: '8', color: 'rgb(89,88,92)' },
+    { name: '9', color: 'rgb(89,88,92)' },
+    { name: '×', color: 'rgb(240,155,44)' },
+    { name: '4', color: 'rgb(89,88,92)' },
+    { name: '5', color: 'rgb(89,88,92)' },
+    { name: '6', color: 'rgb(89,88,92)' },
+    { name: '-', color: 'rgb(240,155,44)' },
+    { name: '1', color: 'rgb(89,88,92)' },
+    { name: '2', color: 'rgb(89,88,92)' },
+    { name: '3', color: 'rgb(89,88,92)' },
+    { name: '+', color: 'rgb(240,155,44)' },
+    { name: '0', color: 'rgb(89,88,92)' },
+    { name: ',', color: 'rgb(89,88,92)' },
+    { name: '=', color: 'rgb(240,155,44)' },
   ].reverse()
-  //Table
-  //-> NumberField
-  //-> Clear  ±  %  ÷
-  //-> 7 8 9 ×
-  //-> 4 5 6 -
-  //-> 1 2 3 +
-  //->  0  , =
+
   const table = document.createElement('table')
   calculatorDiv.appendChild(table)
   for (let i = 0; i < 6; i++) {
@@ -38,21 +32,24 @@ const initializeCalculatorHTML = () => {
     if (i != 0 && i != 5) {
       for (let j = 0; j < 4; j++) {
         const tableData = document.createElement('td')
-        const corresprondingSymbol = symbols.pop()
-        tableData.classList.add(corresprondingSymbol, 'calculatorElement')
-        tableData.innerHTML = corresprondingSymbol
+        const corresprondingSymbol = calculatorProperties.pop()
+        tableData.classList.add(corresprondingSymbol.name, 'calculatorElement')
+        tableData.innerHTML = corresprondingSymbol.name
+        tableData.style.backgroundColor = corresprondingSymbol.color
         tableRow.appendChild(tableData)
       }
     } else if (i == 0) {
       const tableData = document.createElement('td')
       tableData.classList.add('inputElement')
+      tableData.style.backgroundColor = 'rgb(35,33,38)'
       tableRow.appendChild(tableData)
     } else if (i == 5) {
       for (let j = 0; j < 3; j++) {
         const tableData = document.createElement('td')
-        const corresprondingSymbol = symbols.pop()
-        tableData.classList.add(corresprondingSymbol, 'calculatorElement')
-        tableData.innerHTML = corresprondingSymbol
+        const corresprondingSymbol = calculatorProperties.pop()
+        tableData.classList.add(corresprondingSymbol.name, 'calculatorElement')
+        tableData.innerHTML = corresprondingSymbol.name
+        tableData.style.backgroundColor = corresprondingSymbol.color
         tableRow.appendChild(tableData)
       }
     }
